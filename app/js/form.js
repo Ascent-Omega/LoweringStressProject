39302ff3c9bc4c75616c2f75d15f60122d54655a
dteofilovic <davidt995@gmail.com>
2016-05-24 17:06:13 -0700

    Added map to index page
    -Added a map to the page but nothing to interact with it.

--- app/js/form.js
+++ app/js/form.js
@@ -2,3 +2,12 @@ function getData() {
 	var beginning_dest 	= $('input#beginning_destination').val();
 	var ending_dest		= $('input#ending_destination').val();
 }
+
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+	center: {lat: -34.397, lng: 150.644},
+	zoom: 8
+  });
+}
