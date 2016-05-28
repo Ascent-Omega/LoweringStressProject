b1ad073f7ddd72a197de4be846c5c63122fa5789
Ascent-Omega <patelhim@onid.oregonstate.edu>
2016-05-28 13:29:07 -0400

    added checkbox to select cheapest values on click

--- Project B/travel_app/views/public/js/selectScript.js
+++ Project B/travel_app/views/public/js/selectScript.js
@@ -7,8 +7,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	populateVehicle();
 	populateGas();
 	testNumericalQuery();
-  });
-  
+	
+ });
+
 var selectHotel = document.getElementById("selectHotel");
 var selectVehicle = document.getElementById("selectVehicle");
 var selectNights = document.getElementById("selectNights");
@@ -19,6 +20,63 @@ var allowH = true;
 var allowN = true;
 var allowV = true;
 var allowG = true;
+
+document.getElementById("cheapChk").addEventListener("click", checkClick);
+selectHotel.addEventListener("click", selectClicked);
+selectNights.addEventListener("click", selectClicked);
+selectGas.addEventListener("click", selectClicked);
+
+function checkClick(event){
+        
+	// choose cheapest hotel price
+	var totalOptions = selectHotel.getElementsByTagName("option").length;
+	//console.log(totalOptions);
+	
+	var lowestValue = getLowestValue(selectHotel,totalOptions);
+	
+	//selectHotel.querySelectorAll("option[value=" + lowestValue + "]");
+	selectHotel.value = lowestValue;
+	
+	// choose cheapest nights(1)
+	totalOptions = selectNights.getElementsByTagName("option").length;
+	lowestValue = getLowestValue(selectNights,totalOptions);
+	selectNights.value = lowestValue;
+	
+	// choose cheapest gas price
+	totalOptions = selectGas.getElementsByTagName("option").length;
+	lowestValue = getLowestValue(selectGas,totalOptions);
+	selectGas.value = lowestValue;
+}
+
+function getLowestValue(element, totalOptions){
+        
+	var lowest = Number(element[1].value);
+	
+	var x = 15;
+	loop();
+	function loop() {
+		for (x=1; x<= ((Number(totalOptions))-1); x++) {
+			
+			if((Number(element[x].value)) < lowest){
+				lowest = Number(element[x].value);
+			}
+			//console.log(" running");
+
+		}
+	}
+	
+	//console.log("af8 running");
+
+	//console.log(lowest);
+	
+	return lowest;
+}
+
+function selectClicked(){
+        // uncheck
+        document.getElementById("cheapChk").checked = false;
+
+}
 
 // verifies whether the options generated the proper values
 function executeHTests(){
@@ -208,8 +266,6 @@ if(allowH){
 		
 		payload = response;
 		
-
-		
 		console.log(response);
 		//alert(response.name[0]);
 		selectHOption(response);
@@ -251,8 +307,6 @@ if(allowN){
 		
 		payload = response;
 		
-
-		
 		console.log(response);
 		//alert(response.name[0]);
 		selectNOption(response);
@@ -292,9 +346,7 @@ if(allowV){
 		//document.getElementById('status').textContent = response;
 		
 		payload = response;
-		
-
-		
+				
 		console.log(response);
 		//alert(response.name[0]);
 		selectVOption(response);
@@ -336,8 +388,6 @@ if(allowG){
 		
 		payload = response;
 		
-
-		
 		console.log(response);
 		//alert(response.name[0]);
 		selectGOption(response);
@@ -361,14 +411,15 @@ function testNumericalQuery(){
 	
 	travelDistance = Number(travelDistance);
 	
-	if(isNaN(travelDistance)){
-		console.log("Query is not a number - " + travelDistance);
+	if((isNaN(travelDistance)) || travelDistance === 0){
+		//console.log("Query is not a number - " + travelDistance);
 		
 		document.getElementById("warn").innerHTML = "You got here the wrong way. Click the link to get to the right page";
 		var url = "http://52.11.246.123:3000";
 		var result = url.link("http://52.11.246.123:3000");
 		document.getElementById("showHomepage").innerHTML = result;
 		
+		document.getElementById("select-container").style.display = "none";
 		document.getElementById("calc").style.display = "none";
 	}
 	else
@@ -385,176 +436,3 @@ calculateBtn.addEventListener('click', function(event){
 	
 	//console.log(travelDistance);
 });
-/*
-function works(input){
-	var dateObj = {};
-	for (key in input) {
-	  var obj = input[key];
-	  var day = obj.DayPrice;
-	  for (dt in day) {
-		var dtObj = day[dt];
-		var dtKey = dtObj.Date;    
-		if (dateObj.hasOwnProperty(dtKey)) {
-			dateObj[dtKey].push({ Code: obj.attributes.Code, Rate: dtObj.Rate });
-		} else {
-			dateObj[dtKey] = [{ Code: obj.attributes.Code, Rate: dtObj.Rate }];
-		}
-	  }
-	}
-
-	for(d in dateObj) {
-		var obj = dateObj[d];
-		var row = '<tr><td>' + d + '</td><td><ul>';
-	  $.each(obj, function(i, val) {
-		console.log(val);
-		row += '<li>' + val.Code + ': ' + val.Rate + '</li>';
-	  });
-	  row += '</ul></td></tr>';
-	  $('#target').find('tbody').append(row);
-	}
-}
-
-
-function loadJSON2(jsonObj, key, selectId, appendTarget) {
-  // Get the target to append
-  appendTarget = appendTarget ? document.querySelector(appendTarget) : document.body;
-  var arr = JSON.parse(jsonObj);
-  // Create select and set id.
-  var select = document.createElement('select');
-  if (selectId != null) {
-      select.id = selectId;
-  }
-  
-  // Loop through array
-  arr.forEach(function(item) {
-    var option = document.createElement('option');
-    option.text = item[key];
-    select.add(option);
-  });
-
-  appendTarget.appendChild(select);
-}
-
-function makeTable() {
-  var req = new XMLHttpRequest();
-  var requestString = url + "/select";
-  req.open("GET", requestString, true);
-  // Create a load event for the AJAX request (asychronous)
-  req.addEventListener('load', function() {
-    // Check to make sure valid response is received
-    if (req.status >= 200 && req.status < 400) {
-      // Parse out the JSON information
-      var infoReceived = JSON.parse(req.responseText);
-	  console.log("Look Here: "+infoReceived);
-		//works(infoReceived);
-		//loadJSON2(infoReceived);
-      //constructTable(infoReceived);
-    } else {
-      // If a server error was received, post the response text to the log
-      console.log("Error in network request: " + request.statusText);
-    }
-  });
-  // Send the request and prevent default refresh
-  req.send(null);
-}
-/*get elements
-var hotel_name = document.getElementById('name');
-var price_per_night = document.getElementById('room_price');
-var submitBtn = document.getElementById('submitBtn');
-
-// was working here.
-/
-//creates an html table within a form to store workout data
-//content - server response from a sequel query
-function makeTable(content){
-    //get table elements and create row
-    var table = document.getElementById('hotelTable');
-    var header = document.getElementById('header');
-    var row = document.createElement('tr');
-
-    table.hidden = false; //hide the table
-
-    //if there is no header row, then create one
-    if(header == null){
-        var headerRow = document.createElement('tr');
-        headerRow.id = 'header';
-        table.appendChild(headerRow);
-
-        for(prop in content[0]){
-            if(prop != 'id') {
-                var newHeader = document.createElement('th');
-                headerRow.appendChild(newHeader);
-                newHeader.textContent = prop.toString().toUpperCase();
-            }
-        }
-    }
-
-    //create a hidden input and append it to the latest row
-    table.appendChild(row);
-    var hiddenField = document.createElement('input');
-    hiddenField.type = 'hidden';
-    hiddenField.id = content[0].id;
-    hiddenField.className = 'hiddenInput';
-    row.appendChild(hiddenField);
-
-    //create table cells and fill them with textboxes occupied by the submitted form data
-    for (prop in content[0]) {
-        if (prop != 'id') {
-            var data = document.createElement('td');
-            var field = document.createElement('input');
-            row.appendChild(data);
-            data.appendChild(field);
-            field.type = 'text';
-            field.value = content[0][prop];
-            field.id = prop.toString() + hiddenField.id;
-        }
-    }
-
-    //print a status message and return an object containing the current row and id
-    document.getElementById('status').textContent = 'Status: Row successfully inserted into database.';
-    return {row: row, hidden: hiddenField.id};
-}
-
-//form submit button event listener
-submitBtn.addEventListener('click',function(event){
-    var btnType = document.getElementById('insert'); //get form submit button
-    var valid = true; //boolean flag indicating whether or not all form fields are filled
-
-    //form a AJAX request and its content
-    var request = new XMLHttpRequest();
-    var payload = {
-        name: hotel_name.value,
-        room_price_per_night: price_per_night.value,
-        btnType: btnType.name
-    };
-
-    //ensure all form fields have data
-    for(prop in payload){
-        if(payload[prop] == ""){
-            valid = false;
-            document.getElementById('status').textContent = 'Status: Unable to insert row. Make sure all form fields have data.'
-        }
-    }
-
-    //send the request if all form fields have data, then insert the row into the html and database table
-    if(valid) {
-
-        request.open('POST', '/', true);
-
-        request.addEventListener('load', function () {
-            if (request.status >= 200 && request.status < 400) {
-                var response = JSON.parse(request.responseText);
-                insertRow(response);
-            }
-        });
-
-        //send the request
-        request.setRequestHeader("Content-Type", "application/json");
-        request.send(JSON.stringify(payload));
-    }
-
-    event.preventDefault();
-});
-
-makeTable();
-*/
