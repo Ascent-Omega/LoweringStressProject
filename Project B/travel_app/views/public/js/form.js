5ed5f3ed0fb698e500af88d20cbad2db6c4eb35f
dteofilovic <davidt995@gmail.com>
2016-05-27 18:25:23 -0700

    Add some styles to /select

--- Project B/travel_app/views/public/js/form.js
+++ Project B/travel_app/views/public/js/form.js
@@ -3,27 +3,8 @@ function getData() {
 	var ending_dest		= $('input#ending_destination').val();
 	var beginning_dest_loc = getLatLong(beginning_dest);
 	var ending_dest_loc = getLatLong(ending_dest);
-	// console.log(beginning_dest_loc);
 	initMap(beginning_dest_loc,ending_dest_loc);
-	// getDistance(beginning_dest_loc,ending_dest_loc);
 }
-
-// function getDistance(beginning_dest_loc, ending_dest_loc) {
-// var url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + beginning_dest_loc.lat + "," + beginning_dest_loc.lng + "&destinations=" + ending_dest_loc.lat + "," + ending_dest_loc.lng;
-// // console.log(url);
-// var results;
-// $.ajax({
-//  type: "GET",
-//  url: url,
-//  async: false,
-//  success: function(data){
-// 	 console.log(data);
-// 	//  results = data;
-// 	//  results = data.results[0].geometry.location;
-// }
-// });	
-// return results;
-// }
 
 function getLatLong(location) {
 	var url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=AIzaSyDZ0Iz2-uDQLCltYL5zcJYtUsLaSf24wuE";
@@ -35,7 +16,7 @@ function getLatLong(location) {
    success: function(data){
 		 results = data.results[0].geometry.location;
 	}
-	});	
+	});
 	return results;
 }
 
@@ -66,23 +47,23 @@ function initMap(beginning_dest_loc,ending_dest_loc) {
 	     // Display the route on the map.
 	     directionsDisplay.setDirections(response);
 			computeTotalDistance(directionsDisplay.getDirections());
-			 console.log(directionsService);
 	   }
 	 });
-	 
+}
+
 function computeTotalDistance(result) {
   var total = 0;
   var myroute = result.routes[0];
   for (var i = 0; i < myroute.legs.length; i++) {
     total += myroute.legs[i].distance.value;
   }
-  total = total / 1609.344;
-  document.getElementById('total').innerHTML = total + ' miles.';
-	console.log(total);
+  total = Math.round(total / 1609.344);
+  var cost_button = document.getElementById('costs');
+  cost_button.value = total;
+  cost_button.disabled = false;
 }
- 
-  // map = new google.maps.Map(document.getElementById('map'), {
-	// center: {lat: -34.397, lng: 150.644},
-	// zoom: 8
-  // });
+
+function routePage() {
+	var miles = $('button#costs').val();
+	window.location.href = "/select?miles=" + miles;
 }
