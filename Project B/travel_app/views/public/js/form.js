f67c72456d5c9aaf884303d2451c1a0b3d0bc48f
dteofilovic <davidt995@gmail.com>
2016-06-02 16:35:36 -0700

    Style changes

--- Project B/travel_app/views/public/js/form.js
+++ Project B/travel_app/views/public/js/form.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
-	
+
 	populateHotel();
 	populateNights();
-	
+
  });
 
 var selectHotel = document.getElementById("selectHotel");
@@ -20,7 +20,7 @@ selectNights.addEventListener("click", selectClicked);
 
 
 function getData() {
-	
+
 	var beginning_dest 	= $('input#beginning_destination').val();
 	var ending_dest		= $('input#ending_destination').val();
 	var beginning_dest_loc = getLatLong(beginning_dest);
@@ -28,13 +28,13 @@ function getData() {
 	// console.log(beginning_dest_loc);
 	initMap(beginning_dest_loc,ending_dest_loc);
 	// getDistance(beginning_dest_loc,ending_dest_loc);
-	
+
 }
 
 function showConfirm(){
-	
+
 	document.getElementById("confirm").style.display = "";
-	
+
 }
 
 
@@ -48,7 +48,7 @@ function getLatLong(location) {
    success: function(data){
 		 results = data.results[0].geometry.location;
 	}
-	});	
+	});
 	return results;
 }
 
@@ -63,14 +63,14 @@ function initMap(beginning_dest_loc,ending_dest_loc) {
 	 var directionsDisplay = new google.maps.DirectionsRenderer({
 	   map: map
 	 });
-	
+
 	 var mode = document.querySelector('input[name = "transport"]:checked').value;
 	 var pref = document.querySelector('input[name = "preference"]:checked').value;
 	 var mass_trans_type = document.querySelector('input[name = "transport"]:checked').id;
 
 	 //var $radio = $('input[name=transport]:checked');
 	 //var ids = $radio.attr('id');
-	 console.log(mode);	 console.log(pref); console.log(mass_trans_type); 
+	 console.log(mode);	 console.log(pref); console.log(mass_trans_type);
 	 var request;
 	 // Set destination, origin and travel mode.
 	 if( mode == "TRANSIT"){
@@ -114,7 +114,7 @@ function initMap(beginning_dest_loc,ending_dest_loc) {
 		   document.getElementById('total').innerHTML = "";
 	   }
 	 });
-	 
+
 function computeTotalDistance(result) {
   var total = 0;
   var myroute = result.routes[0];
@@ -123,11 +123,11 @@ function computeTotalDistance(result) {
   }
   total = Math.round(total / 1609.344);
   totalMiles = total;
-  document.getElementById('total').innerHTML = total + ' miles.';
+  document.getElementById('total').innerHTML = 'Total distance: ' + total + ' miles.';
 	console.log(total);
-	
+
 }
- 
+
 
   // map = new google.maps.Map(document.getElementById('map'), {
 	// center: {lat: -34.397, lng: 150.644},
@@ -137,13 +137,13 @@ function computeTotalDistance(result) {
 
 function routePage()
 {
-	
+
 	var sendMiles = totalMiles;
 	//console.log("Miles -? " + sendMiles);
 	//console.log(window.location.href);
 	var trans_type = document.querySelector('input[name = "transport"]:checked').id;
 	var ticketPrice;
-	
+
 	if(trans_type == "driving"){
 		goHere = window.location.href + "select?miles=" + sendMiles;
 		//window.location.href = "/select?miles=" + sendMiles;
@@ -159,30 +159,30 @@ function routePage()
 	{
 		document.getElementById("transit-fields").style.display = "";
 	}
-	
-	
+
+
 }
 
 function asyncGetReq(url, res)
 {
     var req = new XMLHttpRequest();
-    req.onreadystatechange = function() { 
+    req.onreadystatechange = function() {
         if (req.readyState == 4 && req.status == 200)
             //res(req.responseText);
 		console.log((req.responseText));
 
     }
-    req.open("GET", url, true); // true for asynchronous 
+    req.open("GET", url, true); // true for asynchronous
     req.send(null);
 }
 
 function confirmButton() {
-	
+
         location.href = goHere;
 };
 
 document.getElementById("submit").addEventListener("click",  function(event) {
-	
+
 	document.getElementById("no-results").style.visibility = "hidden";
 
 });
@@ -212,31 +212,31 @@ $("#driving").click(function(){
 });
 
 function checkClick(event){
-        
+
 	// choose cheapest hotel price
 	var totalOptions = selectHotel.getElementsByTagName("option").length;
 	//console.log(totalOptions);
-	
+
 	var lowestValue = getLowestValue(selectHotel,totalOptions);
-	
+
 	selectHotel.value = lowestValue;
-	
+
 	// choose cheapest nights(1)
 	totalOptions = selectNights.getElementsByTagName("option").length;
 	lowestValue = getLowestValue(selectNights,totalOptions);
 	selectNights.value = lowestValue;
-	
+
 }
 
 function getLowestValue(element, totalOptions){
-        
+
 	var lowest = Number(element[1].value);
-	
+
 	var x = 15;
 	loop();
 	function loop() {
 		for (x=1; x<= ((Number(totalOptions))-1); x++) {
-			
+
 			if((Number(element[x].value)) < lowest){
 				lowest = Number(element[x].value);
 			}
@@ -244,7 +244,7 @@ function getLowestValue(element, totalOptions){
 
 		}
 	}
-	
+
 	return lowest;
 }
 
@@ -255,7 +255,7 @@ function selectClicked(){
 }
 
 function selectHOption(input){
-	
+
 
 	for(var i = 0; i < input.length; i++) {
     var opt = input[i].name + ", " +
@@ -264,13 +264,13 @@ function selectHOption(input){
     hotel.textContent = opt;
     hotel.value = input[i].room_price_per_night;
     selectHotel.appendChild(hotel);
-	
-	
+
+
 	}
 };
 
 function selectNOption(input){
-	
+
 
 	for(var i = 0; i < input.length; i++) {
     var opt = input[i].total_nights;
@@ -278,8 +278,8 @@ function selectNOption(input){
     nights.textContent = opt;
     nights.value = opt;
     selectNights.appendChild(nights);
-	
-	
+
+
 	}
 };
 
@@ -290,26 +290,26 @@ function populateHotel(){
 	var reqString = url + "selectHotel";
 
 	req.open('GET', reqString, true);
-	
+
 	var payload = {};
 
 	req.addEventListener('load',function() {
 	if (req.status >= 200 && req.status < 400) {
 		var response = JSON.parse(req.responseText);
-				
+
 		payload = response;
-		
+
 		selectHOption(response);
-		
+
 	}else {
 			// If a server error was received, post the response text to the log
 			console.log("Error in network request: " + req.statusText);
 		}
 	});
-	
+
 	req.setRequestHeader("Content-Type", "application/json");
     req.send(JSON.stringify(payload));
- 
+
 };
 
 
@@ -320,15 +320,15 @@ function populateNights(){
 	var reqString = url + "selectNights";
 
 	req.open('GET', reqString, true);
-	
+
 	var payload = {};
 
 	req.addEventListener('load',function() {
 	if (req.status >= 200 && req.status < 400) {
 		var response = JSON.parse(req.responseText);
-		
+
 		payload = response;
-		
+
 		selectNOption(response);
 
 	}else {
@@ -336,7 +336,7 @@ function populateNights(){
 			console.log("Error in network request: " + req.statusText);
 		}
 	});
-	
+
 	req.setRequestHeader("Content-Type", "application/json");
     req.send(JSON.stringify(payload));
 
@@ -348,11 +348,11 @@ function doMath(tickCost){
 	var totalNights = getValue(selectNights);
 
 	tickCost = +(tickCost).toFixed(2);
-	
+
 	var stayPrice = +((hotelPrice * totalNights).toFixed(2));
 
 	var totalPrice = +((tickCost + stayPrice).toFixed(2));
-	
+
 	document.getElementById("transPrice").innerHTML = "Transit Price: $" + tickCost;
 	document.getElementById("stayPrice").innerHTML = "Hotel Price: $" + stayPrice;
 	document.getElementById("totalPrice").innerHTML = "Total: $" + totalPrice;
@@ -361,13 +361,13 @@ function doMath(tickCost){
 }
 
 function getValue(element){
-	
+
 	// need tests here for 0 values or null values
 	//if(element == )
-	
+
 	var val = Number(element.options[element.selectedIndex].value);
 	console.log(val);
-	
+
 	return val;
 }
 
@@ -375,7 +375,7 @@ function ticketCost(totalRouteDistance)
 {
 	var driving_BUS_TRAIN = document.querySelector('input[name = "transport"]:checked').id;
 	var ticketCost;
-	
+
 	if(driving_BUS_TRAIN == "BUS")
 	{
 		ticketCost = (getBase2(totalRouteDistance) * 1.25 )* 5.34;
@@ -384,7 +384,7 @@ function ticketCost(totalRouteDistance)
 	{
 		ticketCost = (getBase2(totalRouteDistance) * 1.50 )* 6.34;
 	}
-	
+
 	return ticketCost;
 }
 
@@ -393,7 +393,7 @@ function getBase2(y) {
 }
 
 calculateBtn.addEventListener('click', function(event){
-	
+
     var driving_BUS_TRAIN = document.querySelector('input[name = "transport"]:checked').id;
 
 
@@ -406,12 +406,12 @@ calculateBtn.addEventListener('click', function(event){
 	{
 		ticketPrice = ticketCost(totalMiles);
 		doMath(ticketPrice);
-	}	
+	}
 	//console.log(travelDistance);
 });
 
 function clearPrices(){
-	
+
 	document.getElementById("transPrice").innerHTML = "";
 	document.getElementById("stayPrice").innerHTML = "";
 	document.getElementById("totalPrice").innerHTML = "";
